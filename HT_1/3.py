3e7d771246accda478affec0452f45eff0f9a9bc
valentynhol <88289749+valentynhol@users.noreply.github.com>
2021-11-17 20:21:03 +0200

    Fixed 2, 3

--- HT_1/3.py
+++ HT_1/3.py
@@ -2,12 +2,14 @@
 3. Write a script to sum of the first n positive integers.
 
 """
-n = int(input('Кількість чисел: '))
+
+
+n = int(input('Число: '))
 
 NumberSum = 0
 
 for i in range(n):
-    Number = int(input('Число ' + str(i+1) + ': '))
+    Number = i+1
 
     NumberSum += Number
 
