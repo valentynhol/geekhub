1cb7de2473a6251a0e034c225d8e8218b665b659
Valentyn <valentingolovko2006@gmail.com>
2021-12-02 16:00:35 +0200

    Homework 5 Task 7

--- HT_5/6.py
+++ HT_5/6.py
@@ -1,44 +1,47 @@
+
+  
 """
 6. Всі ви знаєте таку функцію як <range>. Напишіть свою реалізацію цієї функції.
    P.S. Повинен вертатись генератор.
    P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній: https://docs.python.org/3/library/stdtypes.html#range
 """
+
 class MyRangeError(Exception):
         pass
 
 def generator(start, stop, step):
         if step > 0:
-                while True:  
-                        if start < stop:
-                                yield start
-                        else:
-                                break
-                        start += step
+            while True:  
+                if start < stop:
+                    yield start
+                else:
+                    break
+                start += step
         elif step < 0:
-                while True:  
-                        if start > stop:
-                                yield start
-                        else:
-                                break
-                        start += step
+            while True:  
+                if start > stop:
+                    yield start
+                else:
+                    break
+                start += step
 
 
 def my_range(*args):
-        if len(args) == 1:
-                start = 0
-                stop = args[0]
-                step = 1
-        elif len(args) == 2:
-                start = args[0]
-                stop = args[1]
-                step = 1
-        elif len(args) == 3:
-                start = args[0]
-                stop = args[1]
-                step = args[2]
-        else:
-                raise MyRangeError
+    if len(args) == 1:
+        start = 0
+        stop = args[0]
+        step = 1
+    elif len(args) == 2:
+        start = args[0]
+        stop = args[1]
+        step = 1
+    elif len(args) == 3:
+        start = args[0]
+        stop = args[1]
+        step = args[2]
+    else:
+        raise MyRangeError
         
-        return generator(start, stop, step)
+    return generator(start, stop, step)
 
 print([num for num in my_range(100, 17, -2)])
--- HT_5/7.py
+++ HT_5/7.py
@@ -15,18 +15,18 @@
 """
 
 def generator(iterable, *args):
-   for i in iterable:
-      yield i
+    while True:
+        for i in iterable:
+            yield i
 
 select = input('Введіть тип(tuple/list/str)')
 
 if select == "tuple":
-   iterable = tuple(input("Введіть об'єкти через кому: ").split(', '))
+    iterable = tuple(input("Введіть об'єкти через кому: ").split(', '))
 elif select == 'list':
-   iterable = list(input("Введіть об'єкти через кому: ").split(', '))
+    iterable = list(input("Введіть об'єкти через кому: ").split(', '))
 elif select == "str":
-   iterable = input("Введіть рядок: ")
+    iterable = input("Введіть рядок: ")
 
-while True:
-   for i in [element for element in generator(iterable)]:
-      print (i)
+for i in generator(iterable):
+    print(i)
