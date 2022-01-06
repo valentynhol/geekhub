b22ec1b3d54f84ffbfc1abd913632bd64ae2270b
valentyn <valentingolovko2006@gmail.com>
2022-01-06 17:45:31 +0200

    Homework 13

--- HT_13/2.py
+++ HT_13/2.py
@@ -5,28 +5,77 @@
 
 
 class Person(object):
-    def __init__(self, **kwargs):
-        for kwarg in kwargs:
-            exec('self.' + kwarg + " = '" + str(kwargs[kwarg]) + "'")
-        self.all_information = kwargs
+    """
+    Usage:
+        person = Person('name' --> str, 'surname' --> str, 'profession' --> str, 'age' --> int, 'sex'(M or F) --> str)
+
+    Attributes
+    ----------------
+    name --> str(default: None):
+        Represents person's name
+
+    surname --> str(default: None):
+        Represents person's surname
+
+    profession --> str(default: None):
+        Represents people's profession
+
+    age --> int(default: None):
+        Represents people's name
+
+    sex --> str(default: None, 'M' or 'F'):
+        Represents people's sex
+
+    all_information --> dict(default: {}):
+        Contains all info about people
+
+    Methods
+    ----------------
+    show_age(self):
+        Returns self's age
+
+    print_name(self):
+        Prints self's name
+
+    show_all_information(self):
+        Shows all info about self
+    """
+    name = None
+    surname = None
+    age = None
+    profession = None
+    sex = None
+    all_information = {}
+
+    def __init__(self, *args):
+        for arg in args:
+            if arg == 'M' or arg == 'F' and not self.sex:
+                self.sex = arg
+                self.all_information['sex'] = arg
+            elif isinstance(arg, int):
+                self.age = arg
+                self.all_information['age'] = arg
+            elif isinstance(arg, str) and not self.name:
+                self.name = arg
+                self.all_information['name'] = arg
+            elif isinstance(arg, str) and self.name and not self.surname:
+                self.surname = arg
+                self.all_information['surname'] = arg
+            elif isinstance(arg, str) and self.name and self.surname and not self.profession:
+                self.profession = arg
+                self.all_information['profession'] = arg
 
     def show_age(self):
-        try:
-            return self.age
-        except AttributeError:
-            print(None)
+        return self.age
 
     def print_name(self):
-        try:
-            print(self.name, self.surname)
-        except AttributeError:
-            return None
+        print(self.name, self.surname)
 
     def show_all_information(self):
         return self.all_information
 
 
-person1 = Person(profession='Firefighter')
+person1 = Person('Vasya', 'Petiov', 'Firefighter')
 person2 = Person()
 person2.profession = 'Programmer'
 
