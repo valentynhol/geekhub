521af915fcd594f0355a44d88ba5fb25b19ef741
valentyn <valentingolovko2006@gmail.com>
2022-01-12 18:08:01 +0200

    Homework 13

--- HT_14/atm.py
+++ HT_14/atm.py
@@ -4,6 +4,21 @@
 import sqlite3
 
 
+class DatabaseControl(object):
+    def get_wallet_list(self):
+        database = sqlite3.connect('atm.db')
+        cur = database.cursor()
+
+        cur.execute('''SELECT * FROM wallet''')
+        wallet_list = {}
+        fetched = cur.fetchall()
+        database.close()
+        for wal_key in fetched:
+            wallet_list[wal_key[0]] = wal_key[1]
+
+        return wallet_list
+
+
 class User(object):
     role = None
 
@@ -50,43 +65,28 @@ class Admin(User):
             exit()
 
     def check_bank_bal(self):
-        database = sqlite3.connect('atm.db')
-        cur = database.cursor()
-
-        cur.execute('''SELECT * FROM wallet''')
-        wallet_list = {}
-        fetched = cur.fetchall()
-        database.close()
-        for wal_key in fetched:
-            wallet_list[wal_key[0]] = wal_key[1]
+        wallet_list = database_controller.get_wallet_list()
 
         for key in wallet_list:
             print(f'{key}: {wallet_list[key]}')
 
     def add_bank_cash(self):
-        database = sqlite3.connect('atm.db')
-        cur = database.cursor()
-
-        cur.execute('''SELECT * FROM wallet''')
-        wallet_list = {}
-        fetched = cur.fetchall()
-
-        for wal_key in fetched:
-            wallet_list[wal_key[0]] = wal_key[1]
+        wallet_list = database_controller.get_wallet_list()
 
         add_wallet = input('Введіть розмір банкнот, які хочете додати на баланс(10/20/50/100/200/500/1000): ')
 
         if add_wallet not in ['10', '20', '50', '100', '200', '500', '1000']:
             print('Введіть дійсний розмір банкнот!')
-            database.close()
             exit()
 
         add_num = int(input('Введіть к-ть банкнот: '))
 
         if add_num < 0:
             print('Введіть додатню к-ть!')
-            database.close()
             exit()
+
+        database = sqlite3.connect('atm.db')
+        cur = database.cursor()
 
         wallet_list[add_wallet] += add_num
         cur.execute('''UPDATE wallet SET wallet_num=? WHERE wallet=?''', (wallet_list[add_wallet], add_wallet))
@@ -151,19 +151,130 @@ class DefaultUser(User):
         else:
             print('Вкажіть справжню суму.')
 
+    def __basic_wallet_list_create(self, cash, wallet_list, user):
+        wallet_used = {'1000': 0, '500': 0, '200': 0, '100': 0, '50': 0, '20': 0, '10': 0}
+        if wallet_list == wallet_used:
+            print('Вибачте, на даний момент, в банкоматі немає грошей.')
+            default_user.start(user)
+
+        if cash % 10 == 0:
+            if not wallet_list['10'] == 0:
+                if cash % 100 == 10 or cash % 100 == 30:
+                    wallet_used['20'] += cash % 100 // 20
+                    wallet_used['10'] += 1
+
+                elif cash % 100 == 20:
+                    wallet_used['20'] += cash % 100 // 20
+
+                elif cash % 100 == 40:
+                    wallet_used['20'] += 2
+
+                elif cash % 100 == 50 or cash % 100 == 70:
+                    wallet_used['50'] += 1
+                    wallet_used['20'] += (cash % 100 - 50) // 20
+
+                elif cash % 100 == 90:
+                    wallet_used['50'] += 1
+                    wallet_used['20'] += 2
+
+                elif cash % 100 == 60 or cash % 100 == 80:
+                    wallet_used['50'] += 1
+                    wallet_used['20'] += (cash % 100 - 60) // 20
+                    wallet_used['10'] += 1
+
+                cash = cash - cash % 100
+
+                for wallet in wallet_list:
+                    wallet_number = int(cash // int(wallet))
+                    cash = cash % int(wallet)
+
+                    if wallet_number > 0:
+                        wallet_used[wallet] += wallet_number
+
+            elif wallet_list['10'] == 0 and not wallet_list['20'] == 0:
+                if cash == 10 or cash == 30:
+                    print('Не можливо видати')
+                    default_user.start(user)
+
+                if cash % 100 == 10 or cash % 100 == 30:
+                    wallet_used['100'] -= 1
+                    wallet_used['50'] += 1
+                    wallet_used['20'] += (50 + cash % 100) // 20
+
+                elif cash % 100 == 20:
+                    wallet_used['20'] += cash % 100 // 20
+
+                elif cash % 100 == 40:
+                    wallet_used['20'] += 2
+
+                elif cash % 100 == 50 or cash % 100 == 70:
+                    wallet_used['50'] += 1
+                    wallet_used['20'] += (cash % 100 - 50) // 20
+
+                elif cash % 100 == 90:
+                    wallet_used['50'] += 1
+                    wallet_used['20'] += (cash % 100 - 50) // 20
+
+                elif cash % 100 == 60 or cash % 100 == 80:
+                    wallet_used['20'] += cash % 100 // 20
+
+                cash = cash - cash % 100
+
+                for wallet in wallet_list:
+                    wallet_number = int(cash // int(wallet))
+                    cash = cash % int(wallet)
+
+                    if wallet_number > 0:
+                        wallet_used[wallet] += wallet_number
+            else:
+                if cash % min(map(int, wallet_list.keys())) == 0:
+                    for wallet in wallet_list:
+                        wallet_number = int(cash // int(wallet))
+                        cash = cash % int(wallet)
+
+                        if wallet_number > 0:
+                            wallet_used[wallet] += wallet_number
+                else:
+                    print('Неможливо видати.')
+                    default_user.start(user)
+        else:
+            print('Введіть число, яке ділиться на 10.')
+            default_user.start(user)
+
+        return wallet_used
+
+    def __wallet_changer(self, wallet_used, user, wallet_change_count, i):
+        for wallet_change_cycle in range(wallet_change_count[i]):
+            if i == '500' or i == '50':
+                if wallet_change_count[i] % 2 == 0:
+                    if i == '500':
+                        wallet_used['200'] += int(500 * wallet_change_count[i] / 200)
+
+                    else:
+                        wallet_used['20'] += int(50 * wallet_change_count[i] / 20)
+                    wallet_used[i] -= wallet_change_count[i]
+                    break
+                else:
+                    try:
+                        new_wallet = default_user.change_wallet(i)
+                        wallet_used[new_wallet[0]] += new_wallet[1]
+                        wallet_used[new_wallet[2]] += new_wallet[3]
+                        wallet_used[i] -= 1
+                    except TypeError:
+                        print('Недостатньо грошей на балансі банкомата')
+                        default_user.start(user)
+            else:
+                try:
+                    new_wallet = default_user.change_wallet(i)
+                    wallet_used[new_wallet[0]] += new_wallet[1]
+                    wallet_used[i] -= 1
+                except TypeError:
+                    print('Недостатньо грошей на балансі банкомата')
+                    default_user.start(user)
+        return wallet_used
+
     def get_cash(self, user):
-        # get wallet_list
-        database = sqlite3.connect('atm.db')
-        cur = database.cursor()
-
-        cur.execute('''SELECT * FROM wallet''')
-        wallet_list = {}
-        fetched = cur.fetchall()
-
-        for wal_key in fetched:
-            wallet_list[wal_key[0]] = wal_key[1]
-        database.close()
-        # --------------
+        wallet_list = database_controller.get_wallet_list()
 
         bank_bal = 0
         for key in wallet_list:
@@ -171,11 +282,6 @@ class DefaultUser(User):
             wallet_num = wallet_list[key]
             wallet_list[wallet_name] = int(wallet_num)
             bank_bal += int(wallet_name) * int(wallet_num)
-
-        wallet_used = {'1000': 0, '500': 0, '200': 0, '100': 0, '50': 0, '20': 0, '10': 0}
-        if wallet_list == wallet_used:
-            print('Вибачте, на даний момент, в банкоматі немає грошей.')
-            default_user.start(user)
 
         cash = int(input('Введіть суму, яку ви б хотіли зняти з рахунку: '))
 
@@ -190,126 +296,15 @@ class DefaultUser(User):
 
         if 0 <= cash <= float(balance):
             if balance >= cash or bank_bal >= cash:
-                if cash % 10 == 0:
-                    if not wallet_list['10'] == 0:
-                        if cash % 100 == 10 or cash % 100 == 30:
-                            wallet_used['20'] += cash % 100 // 20
-                            wallet_used['10'] += 1
-
-                        elif cash % 100 == 20:
-                            wallet_used['20'] += cash % 100 // 20
-
-                        elif cash % 100 == 40:
-                            wallet_used['20'] += 2
-
-                        elif cash % 100 == 50 or cash % 100 == 70:
-                            wallet_used['50'] += 1
-                            wallet_used['20'] += (cash % 100 - 50) // 20
-
-                        elif cash % 100 == 90:
-                            wallet_used['50'] += 1
-                            wallet_used['20'] += 2
-
-                        elif cash % 100 == 60 or cash % 100 == 80:
-                            wallet_used['50'] += 1
-                            wallet_used['20'] += (cash % 100 - 60) // 20
-                            wallet_used['10'] += 1
-
-                        cash = cash - cash % 100
-
-                        for wallet in wallet_list:
-                            wallet_number = int(cash // int(wallet))
-                            cash = cash % int(wallet)
-
-                            if wallet_number > 0:
-                                wallet_used[wallet] += wallet_number
-
-                    elif wallet_list['10'] == 0 and not wallet_list['20'] == 0:
-                        if cash == 10 or cash == 30:
-                            print('Не можливо видати')
-                            default_user.start(user)
-
-                        if cash % 100 == 10 or cash % 100 == 30:
-                            wallet_used['100'] -= 1
-                            wallet_used['50'] += 1
-                            wallet_used['20'] += (50 + cash % 100) // 20
-
-                        elif cash % 100 == 20:
-                            wallet_used['20'] += cash % 100 // 20
-
-                        elif cash % 100 == 40:
-                            wallet_used['20'] += 2
-
-                        elif cash % 100 == 50 or cash % 100 == 70:
-                            wallet_used['50'] += 1
-                            wallet_used['20'] += (cash % 100 - 50) // 20
-
-                        elif cash % 100 == 90:
-                            wallet_used['50'] += 1
-                            wallet_used['20'] += (cash % 100 - 50) // 20
-
-                        elif cash % 100 == 60 or cash % 100 == 80:
-                            wallet_used['20'] += cash % 100 // 20
-
-                        cash = cash - cash % 100
-
-                        for wallet in wallet_list:
-                            wallet_number = int(cash // int(wallet))
-                            cash = cash % int(wallet)
-
-                            if wallet_number > 0:
-                                wallet_used[wallet] += wallet_number
-
-                    else:
-                        if cash % min(map(int, wallet_list.keys())) == 0:
-                            for wallet in wallet_list:
-                                wallet_number = int(cash // int(wallet))
-                                cash = cash % int(wallet)
-
-                                if wallet_number > 0:
-                                    wallet_used[wallet] += wallet_number
-
-                        else:
-                            print('Неможливо видати.')
-                            default_user.start(user)
-                else:
-                    print('Введіть число, яке ділиться на 10.')
-                    default_user.start(user)
-
                 wallet_change_count = {'1000': 0, '500': 0, '200': 0, '100': 0, '50': 0, '20': 0, '10': 0}
+
+                wallet_used = default_user.__basic_wallet_list_create(cash, wallet_list, user)
 
                 for i in wallet_used:
                     if wallet_list[i] < wallet_used[i]:
                         wallet_change_count[i] = wallet_used[i] - wallet_list[i]
+                        wallet_used = default_user.__wallet_changer(wallet_used, user, wallet_change_count, i)
 
-                        for wallet_change_cycle in range(wallet_change_count[i]):
-                            if i == '500' or i == '50':
-                                if wallet_change_count[i] % 2 == 0:
-                                    if i == '500':
-                                        wallet_used['200'] += int(500 * wallet_change_count[i] / 200)
-
-                                    else:
-                                        wallet_used['20'] += int(50 * wallet_change_count[i] / 20)
-                                    wallet_used[i] -= wallet_change_count[i]
-                                    break
-                                else:
-                                    try:
-                                        new_wallet = default_user.change_wallet(i)
-                                        wallet_used[new_wallet[0]] += new_wallet[1]
-                                        wallet_used[new_wallet[2]] += new_wallet[3]
-                                        wallet_used[i] -= 1
-                                    except TypeError:
-                                        print('Недостатньо грошей на балансі банкомата')
-                                        default_user.start(user)
-
-                            else:
-                                try:
-                                    new_wallet = default_user.change_wallet(i)
-                                    wallet_used[new_wallet[0]] += new_wallet[1]
-                                    wallet_used[i] -= 1
-                                except TypeError:
-                                    print('Недостатньо грошей на балансі банкомата')
-                                    default_user.start(user)
                 wallet = {}
                 for wal_dat in wallet_list:
                     wallet[wal_dat] = wallet_list[wal_dat] - wallet_used[wal_dat]
@@ -352,6 +347,7 @@ class DefaultUser(User):
                 print('Недостатньо коштів на рахунку.')
 
 
+database_controller = DatabaseControl()
 unknown_role_user = User()
 admin_user = Admin()
 default_user = DefaultUser()
