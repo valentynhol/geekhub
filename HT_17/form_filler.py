f6049250f53978957cd9b3b1a7f7d723a1de3009
valentyn <valentingolovko2006@gmail.com>
2022-01-21 22:32:15 +0200

    Homework 17

--- HT_17/form_filler.py
+++ HT_17/form_filler.py
@@ -8,18 +8,19 @@ https://docs.google.com/forms/d/e/1FAIpQLScLhHgD5pMnwxl8JyRfXXsJekF8_pDG36XtSEwa
 https://www.selenium.dev/documentation/
 https://chromedriver.chromium.org/downloads
 """
-from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 
 browser = Chrome()
-options = ChromeOptions()
-options.add_argument('--headless')
 browser.get('https://docs.google.com/forms/d/e/1FAIpQLScLhHgD5pMnwxl8JyRfXXsJekF8_pDG36XtSEwaGsFdU2egyw/viewform?usp=sf_link')
+
 text_field = browser.find_element(By.CLASS_NAME, 'exportInput')
 text_field.click()
 text_field.send_keys('Валентин Головко')
+
 browser.save_screenshot('filled_form.png')
+
 submit_button = browser.find_element(By.CSS_SELECTOR, 'span.exportLabel')
 submit_button.click()
+
 browser.save_screenshot('submitted_form.png')
