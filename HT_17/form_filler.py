0e390ec3377afae4ec012c077143ba309e0d2641
valentyn <valentingolovko2006@gmail.com>
2022-01-22 11:36:48 +0200

    Homework 17

--- HT_17/form_filler.py
+++ HT_17/form_filler.py
@@ -10,11 +10,17 @@ https://chromedriver.chromium.org/downloads
 """
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 browser = Chrome()
 browser.get('https://docs.google.com/forms/d/e/1FAIpQLScLhHgD5pMnwxl8JyRfXXsJekF8_pDG36XtSEwaGsFdU2egyw/viewform?usp=sf_link')
 
+wait = WebDriverWait(browser, 20)
 text_field = browser.find_element(By.CLASS_NAME, 'exportInput')
+
+wait.until(expected_conditions.element_to_be_clickable(text_field))
+
 text_field.click()
 text_field.send_keys('Валентин Головко')
 
@@ -24,3 +30,5 @@ submit_button = browser.find_element(By.CSS_SELECTOR, 'span.exportLabel')
 submit_button.click()
 
 browser.save_screenshot('submitted_form.png')
+
+browser.quit()
