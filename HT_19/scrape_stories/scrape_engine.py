b407969cbf0d2648ba30bf3b47f5944c703fc1ba
valentyn <valentingolovko2006@gmail.com>
2022-02-02 18:58:28 +0200

    Homework 19

--- HT_19/scrape_stories/scrape_engine.py
+++ HT_19/scrape_stories/scrape_engine.py
@@ -3,17 +3,19 @@ import requests
 
 class HackerNews(object):
     @staticmethod
-    def get_list(category):
+    def get_list(category, obj_list):
         ids = requests.get(f'https://hacker-news.firebaseio.com/v0/'
                            f'{category}.json').json()
 
         field_dicts = []
-
+        scraped_count = 0
         for id_num in ids:
-            stories = HackerNews.__get_by_id(id_num)
-            field_dicts.append(stories)
+            if id_num not in obj_list:
+                stories = HackerNews.__get_by_id(id_num)
+                field_dicts.append(stories)
+                scraped_count += 1
 
-        return field_dicts
+        return field_dicts, scraped_count
 
     @staticmethod
     def __get_by_id(stories_id):
--- HT_19/scrape_stories/views.py
+++ HT_19/scrape_stories/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from .scrape_engine import HackerNews
 from .models import Ask, New, Job, Show
-from django.db import IntegrityError
 
 
 def select(request):
     if request.method == 'POST':
         category = request.POST['category_dropdown']
-
-        items = HackerNews.get_list(category)
 
         if category == 'newstories':
             model = New
@@ -19,51 +16,54 @@ def select(request):
         else:
             model = Show
 
+        obj_list = []
+        for i in model.objects.all():
+            obj_list.append(i.stories_id)
+
+        items, scraped_count = HackerNews.get_list(category, obj_list)
+
         created_count = 0
         for item in items:
-            created = False
             if category == 'jobstories':
-                try:
-                    created = model.objects.get_or_create(by=item['by'] if 'by' in item.keys() else None,
-                                                          stories_id=item['id'],
-                                                          score=item['score'] if 'score' in item.keys() else None,
-                                                          time=item['time'] if 'time' in item.keys() else None,
-                                                          title=item['title'] if 'title' in item.keys() else None,
-                                                          type=item['type'] if 'type' in item.keys() else None,
-                                                          url=item['url'] if 'url' in item.keys() else None,
-                                                          text=item['text'] if 'text' in item.keys() else None)[1]
-                except IntegrityError:
-                    pass
+                created = model.objects.get_or_create(by=item['by'] if 'by' in item.keys() else None,
+                                                      stories_id=item['id'],
+                                                      score=item['score'] if 'score' in item.keys() else None,
+                                                      time=item['time'] if 'time' in item.keys() else None,
+                                                      title=item['title'] if 'title' in item.keys() else None,
+                                                      type=item['type'] if 'type' in item.keys() else None,
+                                                      url=item['url'] if 'url' in item.keys() else None,
+                                                      text=item['text'] if 'text' in item.keys() else None)[1]
             elif category == 'newstories' or category == 'showstories':
-                try:
-                    created = model.objects.get_or_create(by=item['by'] if 'by' in item.keys() else None,
-                                                          descendants=item['descendants'] if 'descendants' in item.keys() else None,
-                                                          stories_id=item['id'],
-                                                          score=item['score'] if 'score' in item.keys() else None,
-                                                          time=item['time'] if 'time' in item.keys() else None,
-                                                          title=item['title'] if 'title' in item.keys() else None,
-                                                          type=item['type'] if 'type' in item.keys() else None,
-                                                          url=item['url'] if 'url' in item.keys() else None,
-                                                          text=item['text'] if 'text' in item.keys() else None)[1]
-                except IntegrityError:
-                    pass
+                created = model.objects.get_or_create(by=item['by'] if 'by' in item.keys() else None,
+                                                      descendants=item['descendants'] if 'descendants' in item.keys() else None,
+                                                      stories_id=item['id'],
+                                                      score=item['score'] if 'score' in item.keys() else None,
+                                                      time=item['time'] if 'time' in item.keys() else None,
+                                                      title=item['title'] if 'title' in item.keys() else None,
+                                                      type=item['type'] if 'type' in item.keys() else None,
+                                                      url=item['url'] if 'url' in item.keys() else None,
+                                                      text=item['text'] if 'text' in item.keys() else None)[1]
             else:
-                try:
-                    created = model.objects.get_or_create(by=item['by'] if 'by' in item.keys() else None,
-                                                          descendants=item['descendants'] if 'descendants' in item.keys() else None,
-                                                          stories_id=item['id'],
-                                                          score=item['score'] if 'score' in item.keys() else None,
-                                                          time=item['time'] if 'time' in item.keys() else None,
-                                                          title=item['title'] if 'title' in item.keys() else None,
-                                                          type=item['type'] if 'type' in item.keys() else None,
-                                                          text=item['text'] if 'text' in item.keys() else None)[1]
-                except IntegrityError:
-                    pass
+                created = model.objects.get_or_create(by=item['by'] if 'by' in item.keys() else None,
+                                                      descendants=item['descendants'] if 'descendants' in item.keys() else None,
+                                                      stories_id=item['id'],
+                                                      score=item['score'] if 'score' in item.keys() else None,
+                                                      time=item['time'] if 'time' in item.keys() else None,
+                                                      title=item['title'] if 'title' in item.keys() else None,
+                                                      type=item['type'] if 'type' in item.keys() else None,
+                                                      text=item['text'] if 'text' in item.keys() else None)[1]
 
             if created:
                 created_count += 1
 
-        return render(request, 'done.html', {'count': len(items), 'created_count': created_count})
+        if items:
+            last_item = items[len(items)-1]['id']
+        else:
+            last_item = None
+
+        return render(request, 'done.html', {'count': len(items),
+                                             'items': [created_item['id'] for created_item in items],
+                                             'last_item': last_item})
 
     else:
         return render(request, 'select/scrape_category.html')
